f35cf46c45c507d3f7afa84d2db860a206aecebe
Erica Nafziger <ericakait@Ericas-iMac.local>
2017-01-20 09:14:16 -0800

    create record meal which pushes to the master meal list array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class="container">
     <h1>Meal Tracker App</h1>
+    <new-meal (newMealSender)="addMeal($event)"></new-meal>
+    <h4>Meal List</h4>
+    <div *ngFor="let currentMeal of masterMealList">
+      <p>{{currentMeal.name}}</p>
+      <p>{{currentMeal.details}}</p>
+      <p>{{currentMeal.calories}}</p>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
 
-}
+  masterMealList: Meal[] = [
+    new Meal("Pizza", "2 slices of combination pizza from Costco", 400),
+    new Meal("Salad", "Homemade cobb salad", 220),
+    new Meal("Hamburger", "Hamburger with gluten free bun", 500)
+  ];
 
-export class Meal {
-  public done: boolean = false;
-  constructor(public name: string, public details: string, public calories: string) { }
+  addMeal(newMealFromChild: Meal) {
+    this.masterMealList.push(newMealFromChild);
+  }
+
 }
---
+++ app/new-meal.component.ts
@@ -0,0 +1,49 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+
+@Component({
+  selector: 'new-meal',
+  template: `
+  <div *ngIf="newMealForm === false">
+    <button (click)="newMealFormShow()">Record Meal</button>
+  </div>
+  <div *ngIf="newMealForm">
+    <h3>New Meal</h3>
+    <form>
+      <label>Food name</label>
+      <input #name>
+      <br>
+      <label>Description</label>
+      <input #description>
+      <br>
+      <label>Calories</label>
+      <input #calories>
+    </form>
+    <button (click)="saveNewMeal(name.value, description.value, calories.value); name.value=''; description.value=''; calories.value=''">Save</button>
+    <button (click)="cancelNewMeal()">Cancel</button>
+  </div>
+  `
+})
+
+export class NewMealComponent {
+  @Output() newMealSender = new EventEmitter();
+
+  newMealForm : boolean = false;
+
+  newMealFormShow() {
+    this.newMealForm = true;
+  }
+  saveNewMeal(name: string, description: string, calories: number) {
+    alert('save form');
+    var newMealToAdd: Meal = new Meal(name, description, calories);
+    this.newMealSender.emit(newMealToAdd);
+    this.newMealForm = false;
+  }
+
+  cancelNewMeal() {
+    alert('cancel form');
+    this.newMealForm = false;
+  }
+
+}
